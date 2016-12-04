ad1daa293804953aaf757c62a9402a77f936b993
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-04 22:12:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   $("#btn-start").click(function() {
     //$("#shopping-list").hide();
     $(this).hide();
-  }
+  });
   
   $('#knop').click(function() {
     //$("#shopping-list").hide();
     $(this).hide();
   });
-                                               
+  
 });
