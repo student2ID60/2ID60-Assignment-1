d93f2498b0e1de912a216714fc1630ce0fa07533
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-05 16:57:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,35 @@ $(document).ready(function() {
   
   $('.btn-add').click(function() {
     /*var newProduct = $('input[name=new-product]').val();
-    var newAmount = $('input[name=new-amount]').val();
-    $('#product-form').reset();
-    $('#amount-form').reset();*/
+    var newAmount = $('input[name=new-amount]').val();*/
+    $('#product-form')[0].reset();
+    $('#amount-form')[0].reset();
     $('#new-item').before("<p>hoi</p>");
     //$(this).parent().remove();
   });
+  
+  
+  //Dropdown menu
+  /*
+  // When the user clicks on the button, toggle between hiding and showing the dropdown content
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  */
   
 });
