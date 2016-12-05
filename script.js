df50c2952b0f5339eaf57568fad10e42ca7dedaa
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-05 16:20:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
   $('.btn-add').click(function() {
     /*var newProduct = $('input[name=new-product]').val();
     var newProduct = $('input[name=new-amount]').val();*/
-    $('input[name=new-product]').reset();
-    $('input[name=new-amount]').reset();
+    $('form[name=form-product]').reset();
+    $('form[name=form-amount]').reset();
     $('#new-item').before("<p>hoi</p>");
     //$(this).parent().remove();
   });
