011cae34616374b18eeddff6ebd9fabedce19d31
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-05 17:24:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
     var newAmount = $('input[name=new-amount]').val();*/
     $('#product-form')[0].reset();
     $('#amount-form')[0].reset();
-    $('#new-item').before("<div class='item row row-centered'><div class='col-sm-2 col-centered product'>" + newProduct + "</div><div class='col-sm-1 col-centered amount'>" + newAmount + "</div><div class='col-sm-1 col-centered btn-remove'><img src='remove-icon.png'/></div></div>");
+    $('#new-item').before("<div>" + newProduct + newAmount + "</div>");
+    //$('#new-item').before("<div class='item row row-centered'><div class='col-sm-2 col-centered product'>" + newProduct + "</div><div class='col-sm-1 col-centered amount'>" + newAmount + "</div><div class='col-sm-1 col-centered btn-remove'><img src='remove-icon.png'/></div></div>");
     //$(this).parent().remove();
   });
   
