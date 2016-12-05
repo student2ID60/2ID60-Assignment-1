781e4c2783259863a2fe8fe714879e14a04f0ae5
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-05 17:40:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ $(document).ready(function() {
     var newAmount = $('input[name=new-amount]').val();*/
     $('#product-form')[0].reset();
     $('#amount-form')[0].reset();
-    $('#new-item').before("<div>" + newProduct + newAmount + "</div>");
+    $('#new-item').before("<p>" + newProduct + "</p>");
     //$('#new-item').before("<div class='item row row-centered'><div class='col-sm-2 col-centered product'>" + newProduct + "</div><div class='col-sm-1 col-centered amount'>" + newAmount + "</div><div class='col-sm-1 col-centered btn-remove'><img src='remove-icon.png'/></div></div>");
     //$(this).parent().remove();
   });
   
   
   //Dropdown menu
-  /*
+  
   // When the user clicks on the button, toggle between hiding and showing the dropdown content
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
+  $("#btn-drop-supermarkets").click(function() {
+    $("#drop-supermarkets").toggleClass("show");
+  });
+  
+  /*
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
