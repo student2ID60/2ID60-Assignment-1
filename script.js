6273b116c1cd01b3dde5d2c1f1dd6e1ad15cc0ff
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-06 22:07:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,19 +35,4 @@ $(document).ready(function() {
     $("#drop-supermarkets").toggleClass("show");
   });
   
-  /*
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = $('.dropdown-content');
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }*/
-  
 });
