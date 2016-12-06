5190d9de082b05f59483e92acb01f1f1701784fb
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-06 13:14:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 $(document).ready(function() {
   
+  // show the list after clicking the start-button
   $('#btn-start').click(function() {
     $('#shopping-list').show();
   });
   
-  /*
-  $('.btn-remove').click(function() {
-    $(this).parent().remove();
-    console.log("test");
-  });
-  */
-  
+  // add the new product to the list after clicking the add-button
   $('.btn-add').click(function() {
+    // there should be filled in something in the form
     if($('input[name=new-product]').val() != "") {
+      // get the values the user set for the product to add
       var newProduct = $('input[name=new-product]').val();
       var newAmount = $('input[name=new-amount]').val();
-      if (newAmount==""){
+      // if there has not been filled in an amount, a "-" will be used
+      if (newAmount=="") {
         newAmount = "-";
-      } 
+      }
+      // reset the forms, so that later another new product can be made easily
       $('#product-form')[0].reset();
       $('#amount-form')[0].reset();
+      // add the new product and amount to the shopping list, as the last item before the line where the forms for a new product are
       $('#new-item').before("\n<div class='item row row-centered'>\n<div class='col-sm-2 col-centered product'>" + newProduct + "</div>\n<div class='col-sm-1 col-centered amount'>" + newAmount + "</div>\n<div class='col-sm-1 col-centered btn-remove'>\n<img src='images/remove-icon.png'/>\n</div>\n</div>\n");
-      //$('#new-item').before("<div class='item row row-centered'><div class='col-sm-2 col-centered product'>" + newProduct + "</div><div class='col-sm-1 col-centered amount'>" + newAmount + "</div><div class='col-sm-1 col-centered btn-remove'><img src='images/remove-icon.png'/></div></div>");
     }
   });
   
+  // remove an item from the shopping list after clicking its remove-button
   $('#shopping-list').on('click','.btn-remove',function() {
-    /*var id = $(this).attr('id');
-    alert(id);*/
     $(this).parent().remove();
-    console.log("test");
   });
   
   
-  //Dropdown menu
-  
-  // When the user clicks on the button, toggle between hiding and showing the dropdown content
+  // after clicking the 'supermarkets'-button, toggle between hiding and showing the dropdown content with the supermarkets by toggling the class "show"
   $("#btn-drop-supermarkets").click(function() {
     $("#drop-supermarkets").toggleClass("show");
   });
