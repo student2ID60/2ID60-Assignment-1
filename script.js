cf6a1cf7810e5afd0caf514dc88e5e352910ec86
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-06 11:44:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
       } 
       $('#product-form')[0].reset();
       $('#amount-form')[0].reset();
-      $('#new-item').before("/n<div class='item row row-centered'>\n<div class='col-sm-2 col-centered product'>" + newProduct + "</div>\n<div class='col-sm-1 col-centered amount'>" + newAmount + "</div>\n<div class='col-sm-1 col-centered btn-remove'>\n<img src='images/remove-icon.png'/>\n</div>\n</div>");
+      $('#new-item').before("\n<div class='item row row-centered'>\n<div class='col-sm-2 col-centered product'>" + newProduct + "</div>\n<div class='col-sm-1 col-centered amount'>" + newAmount + "</div>\n<div class='col-sm-1 col-centered btn-remove'>\n<img src='images/remove-icon.png'/>\n</div>\n</div>");
       //$('#new-item').before("<div class='item row row-centered'><div class='col-sm-2 col-centered product'>" + newProduct + "</div><div class='col-sm-1 col-centered amount'>" + newAmount + "</div><div class='col-sm-1 col-centered btn-remove'><img src='images/remove-icon.png'/></div></div>");
     }
   });
