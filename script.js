5e778b7d94ec5b5672f41e35c8400299e8d5ed40
student2ID60 <student2ID60@users.noreply.github.com>
2016-12-06 12:01:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     }
   });
   
-  $(document).on('click','.btn-remove',function() {
+  $(document).on('click','.btn-add',function() {
     var id = $(this).attr('id');
     alert(id);    
   });
